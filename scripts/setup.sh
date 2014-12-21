5afadee48c0369db0a8b29bddf8ab4a0a2d35fe9
Manos Tsardoulias <etsardou@gmail.com>
2014-12-21 13:00:28 +0200

    Closed #1

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,40 @@
 cd ../
+
+
+# Check for removed packages
+exec 4<scripts/packages_legacy
+while read LINE <&4; do
+  if ! grep -Fxq "$LINE" scripts/packages
+  then
+    module=$(echo $LINE | awk -F"/" '{print $NF}')
+    module=bundle/$module
+    module=${module:0:-4}
+
+    # Remove the submodule manually
+    git config -f .git/config --remove-section submodule.$module
+    git config -f .gitmodules --remove-section submodule.$module
+    git rm --cached $module
+    rm -rf $module
+    rm -rf .git/modules/$module
+    echo ">>> $module removed"
+  fi
+done
+
 git submodule init
 
+#Update the packages
 exec 4<scripts/packages
-
 while read LINE <&4; do
-  
   # Get last part of repo (*.git) 
   module=$(echo $LINE | awk -F"/" '{print $NF}')
-  echo ">> Cloning $module"
+  echo ">>> Cloning $module"
   # Remove .git
   module=${module:0:-4}
   # module is the folder's name
-  
   git submodule add $LINE bundle/$module/
 done
+
+#Copy the new packages to legacy file
+cp scripts/packages scripts/packages_legacy
 
 git submodule update
